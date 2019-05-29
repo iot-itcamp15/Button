1a982371e735e19d7ef7fe56b9635a5552a15629
นายคณิติน ผลงาม <43027883+meanknt@users.noreply.github.com>
2019-05-29 15:03:45 +0700

    Update switch.ino

--- switch.ino
+++ switch.ino
@@ -1,14 +1,14 @@
 void setup() {
   // put your setup code here, to run once:
-  pinMode(D0, INPUT); //กำหนดขา D0 เป็น input เข้าไปยังบอร์ด
-  pinMode(D1, OUTPUT);//กำหนดขา D0 เป็น OUTPUT ส่งออกจากบอร์ดไปยังอุปกรณ์
+  pinMode(D0, INPUT); //กำหนด D0 เป็น input เข้าไปยังบอร์ด
+  pinMode(D1, OUTPUT);//กำหนด D0 เป็น OUTPUT ส่งออกจากบอร์ดไปยังอุปกรณ์
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-    if(digitalRead(D0)){ //ถ้ากดปุ่ม(อ่านค่าจากขา D0)
-        digitalWrite(D1, HIGH); //ไฟจากขาD1 ติด
+    if(digitalRead(D0)){ //ถ้ากดปุ่ม(อ่านค่าจาก D0)
+        digitalWrite(D1, HIGH); //ไฟจากD1 ติด
     }else{ //ไม่ได้กดปุ่ม
         digitalWrite(D1, LOW); //ไฟดับ
     }
